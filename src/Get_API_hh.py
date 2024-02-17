942e460674610a175c53037240b43d0c8267cc37
Kirill Dmitruk <dmitruk.ks@mail.ru>
2024-02-17 19:46:56 +0300

    Vacancy.py have been created

---
+++ src/Get_API_hh.py
@@ -0,0 +1,35 @@
+import json
+
+import requests
+
+from src.Abstract_class_hh import Abstract_class_hh
+
+
+class Get_API_hh(Abstract_class_hh):
+
+    def __init__(self):
+        """
+        Создаем пустой список all_vacancy,
+        который будет использоваться для хранения информации о вакансиях.
+        """
+        self.all_vacancy = []
+
+    def __repr__(self):
+        """
+        Метод возвращает строку, содержащую все вакансии, которые были получены.
+        """
+        return f"{self.all_vacancy}"
+
+    def get_vacancy_from_api(self, name_vacancy) -> list:
+        """
+        Метод получает информацию о вакансиях с помощью API hh.ru.
+
+        """
+
+        if name_vacancy.isalpha():
+            keys_response = {'text': f'name:Python', 'area': 113, 'per_page': 100, }
+            info = requests.get(f'https://api.hh.ru/vacancies', keys_response)
+            self.all_vacancy = json.loads(info.text)['items']
+            return self.all_vacancy
+        else:
+            return "Vacancy not found"
